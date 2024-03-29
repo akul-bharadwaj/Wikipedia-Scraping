4fd7acd9e57ea7db2806a984ef679a936eff3e2b
Akul Bharadwaj B H <akulb2h@gmail.com>
2024-03-29 23:29:49 +0000

    Update test_core.py

--- test_core.py
+++ test_core.py
@@ -2,4 +2,4 @@ from nlp.core import get_phrases
 
 
 def test_get_phrase():
-    assert "golden state" in get_phrases("Golden State Warriors")
+    assert "golden state" in get_phrases("Royal Challengers Bengaluru")
